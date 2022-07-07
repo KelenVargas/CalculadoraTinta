a9f89c2b06dd83c374cf886e88b998c36e7b5945
Kelen <kelenvargas.senac@gmail.com>
2022-07-07 19:01:46 -0300

    inserindo o resultado na tela com innerhtml

--- script.js
+++ script.js
@@ -3,9 +3,8 @@ function novaPagina(num) {
     url[0]="http://127.0.0.1:5500/Resultado.html";
     url[1]="http://127.0.0.1:5500/Formulario.html";
     window.location=url[num];
-    }
 
-   
+}   
 
     
    // var areaPorta= 0.80*1.9;//area da porta é fixa
@@ -20,13 +19,8 @@ function novaPagina(num) {
         var parede2= (document.getElementById("Altura2").value) * (document.getElementById("Largura2").value);
         var parede3= (document.getElementById("Altura3").value) * (document.getElementById("Largura3").value);
         var parede4= (document.getElementById("Altura4").value) * (document.getElementById("Largura4").value);
+                 
         
-    
-        console.log("Area parede 1: " + parede1);
-        console.log("Area parede 2: " + parede2);
-        console.log("Area parede 3: " + parede3);
-        console.log("Area parede 4: " + parede4);
-
         var areaPorta=(0.80*1.9);//area da porta é fixa  
         Porta1= (document.getElementById("porta1").value) * (areaPorta)
         Porta2= (document.getElementById("porta2").value) * (areaPorta)
@@ -39,15 +33,28 @@ function novaPagina(num) {
         janela3=(document.getElementById("janela3").value) * (areaJanela)
         janela4=(document.getElementById("janela4").value) * (areaJanela)
 
+
         var areaPortaJanela = areaPorta + areaJanela 
         var totalParede = parede1 + parede2 + parede3 + parede4;
         var areaPintar = totalParede - areaPortaJanela;
+        var quantidadeTinta = areaPintar/5;
+        var res = window.document.getElementById('res')
+
+        
+        console.log("Area parede 1: " + parede1);
+        console.log("Area parede 2: " + parede2);
+        console.log("Area parede 3: " + parede3);
+        console.log("Area parede 4: " + parede4);
 
         console.log ("Area da porta e janela é : " + areaPortaJanela);
         console.log (" A area a ser pintada é: " + areaPintar)
        
-
-
+        res.innerHTML= `Sua area total das paredes é: ${totalParede} m<sup>2</sup> <br>
+                        Sua área das janelas e portas é: ${areaPortaJanela} <br>
+                        Sua area a ser pintada é: ${areaPintar} <br>
+                        E você irá precisar de:${quantidadeTinta} litros`
+        
+                     
     }
 
   
