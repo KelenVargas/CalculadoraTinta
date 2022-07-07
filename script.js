578277711f9724ce714207ab918dc15ef6431b71
Kelen <kelenvargas.senac@gmail.com>
2022-07-07 11:31:07 -0300

    ajustando a logica do botão salvar e inserindo a area das portas e janelas

--- script.js
+++ script.js
@@ -8,23 +8,44 @@ function novaPagina(num) {
    
 
     
-    var areaPorta= 0.80*1.9;//area da porta é fixa
-    var areaJanela= 2.00*1.20;// area da janela é fixa
-    var areaPortaJanela = (qtdePorta*areaPorta)+(qtdeJanela*areaJanela);
-    var areaPintar = areaParede - areaPortaJanela;
-    var quantidadeTinta = areaParede/5;
+   // var areaPorta= 0.80*1.9;//area da porta é fixa
+    //var areaJanela= 2.00*1.20;// area da janela é fixa
+    //var areaPortaJanela = (qtdePorta*areaPorta)+(qtdeJanela*areaJanela);
+   // var areaPintar = areaParede - areaPortaJanela;
+    //var quantidadeTinta = areaParede/5;
 
-    function Salvar(num){
-        var parede = new Array();
-        parede[1]= (document.getElementById("Altura").value) * (document.getElementById("Largura").value);
-        parede[2]= (document.getElementById("Altura2").value) * (document.getElementById("Largura2").value);
-        parede[3]= (document.getElementById("Altura3").value) * (document.getElementById("Largura3").value);
-        parede[4]= (document.getElementById("Altura4").value) * (document.getElementById("Largura4").value);
+    function Salvar(){
+       
+        var parede1= (document.getElementById("Altura").value) * (document.getElementById("Largura").value);
+        var parede2= (document.getElementById("Altura2").value) * (document.getElementById("Largura2").value);
+        var parede3= (document.getElementById("Altura3").value) * (document.getElementById("Largura3").value);
+        var parede4= (document.getElementById("Altura4").value) * (document.getElementById("Largura4").value);
+        
     
-        console.log("Area parede 1: " + parede[1]);
-        console.log("Area parede 2 " + parede[2]);
-        console.log("Area parede 3 " + parede[3]);
-        console.log("Area parede 4 " + parede[4]);
+        console.log("Area parede 1: " + parede1);
+        console.log("Area parede 2: " + parede2);
+        console.log("Area parede 3: " + parede3);
+        console.log("Area parede 4: " + parede4);
+
+        var areaPorta=(0.80*1.9);//area da porta é fixa  
+        Porta1= (document.getElementById("porta1").value) * (areaPorta)
+        Porta2= (document.getElementById("porta2").value) * (areaPorta)
+        Porta3= (document.getElementById("porta3").value) * (areaPorta)
+        Porta4= (document.getElementById("porta4").value) * (areaPorta)
+
+        var areaJanela=(2.00*1.20);// area da janela é fixa
+        janela1=(document.getElementById("janela1").value) * (areaJanela)
+        janela2=(document.getElementById("janela2").value) * (areaJanela)
+        janela3=(document.getElementById("janela3").value) * (areaJanela)
+        janela4=(document.getElementById("janela4").value) * (areaJanela)
+
+        var areaPortaJanela = areaPorta + areaJanela 
+        var totalParede = parede1 + parede2 + parede3 + parede4;
+        var areaPintar = totalParede - areaPortaJanela;
+
+        console.log ("Area da porta e janela é : " + areaPortaJanela);
+        console.log (" A area a ser pintada é: " + areaPintar)
+       
 
 
     }
