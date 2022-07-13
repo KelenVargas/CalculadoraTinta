878168c6f12bd27043e40d898148b9f1f8d4606f
Kelen <kelenvargas.senac@gmail.com>
2022-07-13 16:56:01 -0300

    alterando a logica do while para testes

--- script.js
+++ script.js
@@ -9,28 +9,29 @@ function Clique(num) {
        
     function Salvar(){  
         //declarei as variavéis e já inicializei elas com os input e fazendo o calculos de largura * altura          
-        var parede1= (document.getElementById("Altura").value) * (document.getElementById("Largura").value);
-        var parede2= (document.getElementById("Altura2").value) * (document.getElementById("Largura2").value);
-        var parede3= (document.getElementById("Altura3").value) * (document.getElementById("Largura3").value);
-        var parede4= (document.getElementById("Altura4").value) * (document.getElementById("Largura4").value);
+         parede1= (document.getElementById("Altura").value) * (document.getElementById("Largura").value);
+         parede2= (document.getElementById("Altura2").value) * (document.getElementById("Largura2").value);
+         parede3= (document.getElementById("Altura3").value) * (document.getElementById("Largura3").value);
+         parede4= (document.getElementById("Altura4").value) * (document.getElementById("Largura4").value);
 
         // declarei elas com valor fixo já
-        var areaPorta=(0.80*1.9) * (document.getElementById("porta").value) ;//area da porta é fixa  
+         areaPorta=(0.80*1.9) * (document.getElementById("porta").value);//area da porta é fixa  
              
-        var areaJanela=(2.00*1.20) * (document.getElementById("janela").value);// area da janela é fixa
+        areaJanela=(2.00*1.20) * (document.getElementById("janela").value);// area da janela é fixa
         
-        var areaPortaJanela = areaPorta + areaJanela 
+        var areaPortaJanela = areaPorta + areaJanela;
         var totalParede = parede1 + parede2 + parede3 + parede4;
         var areaPintar = totalParede - areaPortaJanela;
-        var quantidadeTinta = areaPintar/5;
-        var res = window.document.getElementById('res')
+        var quantidadeTinta = areaPintar/5;      
+        var res = window.document.getElementById('res');
         
         
        
         res.innerHTML= `Sua area total das paredes é: ${totalParede} m<sup>2</sup> <br>
                         Sua área das janelas e portas é: ${areaPortaJanela} <br>
                         Sua area a ser pintada é: ${areaPintar} <br>
-                        E você irá precisar de:${quantidadeTinta} litros`
+                        E você irá precisar de:${quantidadeTinta} litros<br>`
+                     
         
                      
     }
@@ -38,32 +39,34 @@ function Clique(num) {
    
           // função para calcular quantas latas de tintas eu preciso com base na litragem acima
       function totalTinta(){
+
             var totalParede = parede1 + parede2 + parede3 + parede4;
             var areaPintar = totalParede - areaPortaJanela;
             var quantidadeTinta = areaPintar/5;
-            var areaPortaJanela = areaPorta + areaJanela           
+            var areaPortaJanela = areaPorta + areaJanela;  
+            var res2 = window.document.getElementById('res2');        
                      
-             lataG  =  0 ;
-             lataM  =  0 ;
-             lataP  =  0;
-             lataPP  = 0;
+             lataG  = 0 ;
+             lataM  = 0 ;
+             lataP  = 0;
+             lataPP = 0;
 
              while  ( quantidadeTinta !=  0 ){
             if(quantidadeTinta>18){
-                lataG++
                 quantidadeTinta= quantidadeTinta-18
+                lataG++
             } else if (quantidadeTinta>3.6) {
-                lataM++
                 quantidadeTinta= quantidadeTinta-3.6
+                lataM++
             } else if (quantidadeTinta>2.5) {
-                lataP++
                 quantidadeTinta= quantidadeTinta-2.5
+                lataP++
             } else if (quantidadeTinta>0.5) {
                 lataPP++
                 quantidadeTinta= quantidadeTinta-0.5
             } else if (quantidadeTinta>0) {
-                latasPP++
-                quantidadeTinta= 0
+               quantidadeTinta= 0
+               latasPP++
             } 
             
         }
